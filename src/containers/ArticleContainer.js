8d6eaa120b976044b7df06542bd28b9870326d7a
dotoring <ysw0823g@gmail.com>
2022-10-28 21:32:09 +0900

    front serv

---
+++ src/containers/ArticleContainer.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import { useStore } from '../components/common/store';
+import ArticleListPage from '../components/pages/ArticleListPage';
+
+const ArticleContainer = () => {
+
+  const { articles, getArticles } = useStore();
+  const [load, setLoad] = useState(false);
+
+  useEffect(() => {
+    getArticles();
+  }, [getArticles]);
+
+  useEffect(() => {
+    if (articles.economy.length) {
+      setLoad(true);
+    }
+    else {
+      setLoad(false);
+    }
+  }, [articles]);
+  
+  return (
+    <div>
+      {load && articles.economy.length !== 0 && (<ArticleListPage article={articles} />)}
+    </div>
+  );
+};
+
+export default ArticleContainer;
